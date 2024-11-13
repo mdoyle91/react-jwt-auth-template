f7e0e422c8ff97c34a3c75934b7249294beaf714
mdoyle91 <mike.w.doyle91@gmail.com>
2024-11-13 12:51:39 -0500

    NavBar Created

---
+++ react-jwt-auth-template/src/App.jsx
@@ -0,0 +1,26 @@
+// src/App.jsx
+
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <>
+      <NavBar user={user} />
+      <Routes>
+        {user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+      </Routes>
+    </>
+  );
+};
+
+export default App;
